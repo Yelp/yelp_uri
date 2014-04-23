fedfc626fa931457f7ea84dd65078a177f68d1c3
Anthony Sottile <asottile@umich.edu>
2014-04-23 11:07:56 -0700

    Add some missing fields to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     name='yelp_uri',
     version='0.1.0',
     description="Uri utilities maintained by Yelp",
+    url='https://github.com/Yelp/yelp_uri',
     author='Buck Golemon',
+    author_email='buck@yelp.com',
     platforms='all',
     classifiers=[
         'License :: Public Domain',
