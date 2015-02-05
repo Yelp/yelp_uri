edf2a1a47355cb459b43178633ef21bb8eb2865c
Buck Golemon <buck@yelp.com>
2015-02-05 13:59:35 -0800

    this is v1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 def main():
     setup(
         name='yelp_uri',
-        version='0.1.1',
+        version='1.0.0',
         description="Uri utilities maintained by Yelp",
         url='https://github.com/Yelp/yelp_uri',
         author='Buck Golemon',
