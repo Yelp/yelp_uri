ee9a24e39f79221525db093c66c22055e523d9ca
Buck Evan <buck@yelp.com>
2016-09-14 11:32:22 -0700

    create a url_regex_common_tlds

--- tests/search_test.py
+++ tests/search_test.py
@@ -3,6 +3,7 @@ import six
 
 from yelp_uri.search import email_regex
 from yelp_uri.search import url_regex
+from yelp_uri.search import url_regex_common_tlds
 
 
 def test_email_regex():
@@ -38,123 +39,145 @@ def test_email_regex():
     assert_no_email('Log in with http://guest@example.com...')
 
 
-def assert_finds_url(text, url):
-    match = url_regex.search(text)
-    assert match is not None, "Expected url_regex match on " + url + " (text: '" + text + "')"
-    assert match.group() == url
+class RegexAssertion(object):
+    def __init__(self, url_regex):
+        self.url_regex = url_regex
 
-def assert_finds_whole_url(url):
-    assert_finds_url(url, url)
+    def assert_finds_url(self, text, url):
+        match = self.url_regex.search(text)
+        assert match is not None, "Expected url_regex match on " + url + " (text: '" + text + "')"
+        assert match.group() == url
 
-def assert_no_url(text):
-    assert url_regex.match(text) is None
+    def assert_finds_whole_url(self, url):
+        self.assert_finds_url(url, url)
 
-def test_url_regex():
-    """Test that our url-matching regex and fast url/email matching regex work properly."""
+    def assert_no_url(self, text):
+        assert self.url_regex.match(text) is None
 
-    assert_finds_url('dictionary.com\'s', 'dictionary.com')
-    assert_finds_url('yelp.com.', 'yelp.com')
-    assert_finds_url('http://sensorydeprivation.co.uk is the person to talk to', 'http://sensorydeprivation.co.uk')
-    assert_finds_url('Read https://www.girlgeniusonline.com', 'https://www.girlgeniusonline.com')
-    assert_finds_url('look at this site with com in it: http://news.ycombinator.com', 'http://news.ycombinator.com')
-    assert_finds_url('My site is http://example.com.I am bad at spacebar.', 'http://example.com')
-    assert_finds_url('Check out http://I.have.com?query!', 'http://I.have.com?query')
-    assert_finds_url('Check out http://I.have.com#!she/bang!', 'http://I.have.com#!she/bang')
-    assert_finds_url('Follow @YelpCincy on Twitter (http://twitter.com/YelpCincy)', 'http://twitter.com/YelpCincy')
-    assert_finds_url(
+
+def assert_url_regex(a):
+
+    a.assert_finds_url('dictionary.com\'s', 'dictionary.com')
+    a.assert_finds_url('yelp.com.', 'yelp.com')
+    a.assert_finds_url('http://sensorydeprivation.co.uk is the person to talk to', 'http://sensorydeprivation.co.uk')
+    a.assert_finds_url('Read https://www.girlgeniusonline.com', 'https://www.girlgeniusonline.com')
+    a.assert_finds_url('look at this site with com in it: http://news.ycombinator.com', 'http://news.ycombinator.com')
+    a.assert_finds_url('My site is http://example.com.I am bad at spacebar.', 'http://example.com')
+    a.assert_finds_url('Check out http://I.have.com?query!', 'http://I.have.com?query')
+    a.assert_finds_url('Check out http://I.have.com#!she/bang!', 'http://I.have.com#!she/bang')
+    a.assert_finds_url('Follow @YelpCincy on Twitter (http://twitter.com/YelpCincy)', 'http://twitter.com/YelpCincy')
+    a.assert_finds_url(
         'Reference: http://en.wikipedia.org/wiki/Eon_(geology)',
         'http://en.wikipedia.org/wiki/Eon_(geology)',
     )
-    assert_finds_url('http://example.com:8O80', 'http://example.com')
-    assert_finds_url('notemail@https://example.com', 'https://example.com')
-    assert_finds_url('notemail@www.example.com', 'www.example.com')
+    a.assert_finds_url('http://example.com:8O80', 'http://example.com')
+    a.assert_finds_url('notemail@https://example.com', 'https://example.com')
+    a.assert_finds_url('notemail@www.example.com', 'www.example.com')
 
-    assert_finds_whole_url('http://example.com:8080')
-    assert_finds_whole_url('http://lil_jay78.blogspot.com/')
-    assert_finds_whole_url('http://yelp.com/berkeley')
-    assert_finds_whole_url('www.audrey_tsang.com')
-    assert_finds_whole_url('http://a.com')
-    assert_finds_whole_url('http://who_even_uses_dot_mobi.mobi')
-    assert_finds_whole_url(u'http://www.yelp.com/münchen')
-    assert_finds_whole_url(u'http://www.ü.com/ü;ü;ü/ü;ü;ü?ü=ü&ü=ü#!ü/ü')
-    assert_finds_whole_url(u'http://➡.ws/➡;➡;➡/➡;➡;➡?➡=➡&➡=➡#!➡/➡')
-    assert_finds_whole_url('http://y.combinator')
-    assert_finds_whole_url('http://.ocregion.com')
+    a.assert_finds_whole_url('http://example.com:8080')
+    a.assert_finds_whole_url('http://lil_jay78.blogspot.com/')
+    a.assert_finds_whole_url('http://yelp.com/berkeley')
+    a.assert_finds_whole_url('www.audrey_tsang.com')
+    a.assert_finds_whole_url('http://a.com')
+    a.assert_finds_whole_url('http://who_even_uses_dot_mobi.mobi')
+    a.assert_finds_whole_url(u'http://www.yelp.com/münchen')
+    a.assert_finds_whole_url(u'http://www.ü.com/ü;ü;ü/ü;ü;ü?ü=ü&ü=ü#!ü/ü')
+    a.assert_finds_whole_url(u'http://➡.ws/➡;➡;➡/➡;➡;➡?➡=➡&➡=➡#!➡/➡')
+    a.assert_finds_whole_url('http://y.combinator')
+    a.assert_finds_whole_url('http://.ocregion.com')
 
     # Chops off everything after last tld, www.dmv.ca
-    assert_finds_url('https://www.dmv.ca.gov\\notpartofurl', 'https://www.dmv.ca.gov')
+    a.assert_finds_url('https://www.dmv.ca.gov\\notpartofurl', 'https://www.dmv.ca.gov')
     # Chops off path after www.dmv.ca
-    assert_finds_url(
+    a.assert_finds_url(
         'Contact the dmv: https://www.dmv.ca.gov/portal/dmv/dmvfooter2/contactus\\nnext_paragraph',
         'https://www.dmv.ca.gov/portal/dmv/dmvfooter2/contactus'
     )
     # Chops off path at the last period, which is the last bad_end
-    assert_finds_url(
+    a.assert_finds_url(
         'Contact the dmv: https://www.dmv.ca.gov/portal/dmv/dmvfooter2/contactus.html\\nnext_paragraph',
         'https://www.dmv.ca.gov/portal/dmv/dmvfooter2/contactus.html'
     )
     # :39 is a line number in this context but is valid path component
-    assert_finds_whole_url('https://media4.com/en_US.min.js:39')
+    a.assert_finds_whole_url('https://media4.com/en_US.min.js:39')
     # Proves :39 is indeed the path component and not the port
-    assert_finds_whole_url('https://media4.com:443/en_US.min.js:39')
+    a.assert_finds_whole_url('https://media4.com:443/en_US.min.js:39')
 
     # Ticket: #31242
     colon_url = 'http://www.yelp.fr/biz/smalls-marseille#hrid:u_UQvMf97E8pD4HEb59uIw'
-    assert_finds_whole_url(colon_url)
-    assert_finds_url('(%s)' % colon_url, colon_url)
+    a.assert_finds_whole_url(colon_url)
+    a.assert_finds_url('(%s)' % colon_url, colon_url)
     colon_url_with_parens = colon_url.replace('#', '#(') + ')'
-    assert_finds_whole_url(colon_url_with_parens)
+    a.assert_finds_whole_url(colon_url_with_parens)
     # This is a real testing domain, from:
     # http://en.wikipedia.org/wiki/List_of_TLDs#Test_TLDS
-    assert_finds_whole_url('http://例子.測試')
-    assert_no_url('http://br@n is the @wesome!!!')
-    assert_no_url('ftp://user@example.com')
+    a.assert_finds_whole_url('http://例子.測試')
+    a.assert_no_url('http://br@n is the @wesome!!!')
+    a.assert_no_url('ftp://user@example.com')
     # These are real reviews that were improperly linkified.
-    assert_no_url(
+    a.assert_no_url(
         'My legs... my legs... must keep going...  must.......be' +
         '.....nearly.....there.....yet....HOLY SHIT IT CONTINUES?')
-    assert_no_url('Great setting however....it was a bit loud.')
-    assert_no_url(
+    a.assert_no_url('Great setting however....it was a bit loud.')
+    a.assert_no_url(
         "I've been greatly disappointed with Gilmore's the last couple years" +
         "...it used to be my go-to spot in the burbs for French."
     )
-    assert_no_url('isemail@emailaddress.com')
-    assert_no_url('org.openqa.selenium.support.events') # events is a tld
-    assert_no_url('EventFiringWebElement.click')        # click is a tld
-    assert_no_url('os.name')                            # name is a tld
-    assert_no_url('main.py')                            # py is a tld
-    assert_no_url('foo.sh')                            # py is a tld
-    assert_no_url('pid.id')                            # py is a tld
+    a.assert_no_url('isemail@emailaddress.com')
 
-    assert_finds_url('http://www.foo.com/blah#foo', 'http://www.foo.com/blah#foo')
-    assert_finds_url('http://www.foo.com/blah!', 'http://www.foo.com/blah')
-    assert_finds_url('http://www.foo.com/blah_(disambiguation)', 'http://www.foo.com/blah_(disambiguation)')
+    a.assert_finds_url('http://www.foo.com/blah#foo', 'http://www.foo.com/blah#foo')
+    a.assert_finds_url('http://www.foo.com/blah!', 'http://www.foo.com/blah')
+    a.assert_finds_url('http://www.foo.com/blah_(disambiguation)', 'http://www.foo.com/blah_(disambiguation)')
+
+    a.assert_finds_whole_url('http://Cervejoteca.com.br')
+
+
+def test_url_regex():
+    """Test that our url-matching regex and fast url/email matching regex work properly."""
+    a = RegexAssertion(url_regex)
+    assert_url_regex(a)
 
     # These are real examples of insurance spam on talk
-    assert_finds_whole_url('LOANFINDERFAST.us')
-    assert_finds_whole_url('LOANFINDERFAST.US')
-    assert_finds_url('LOANFINDERFAST.us.', 'LOANFINDERFAST.us')
-    assert_finds_url('LOANFINDERFAST.us-', 'LOANFINDERFAST.us')
+    a.assert_finds_whole_url('LOANFINDERFAST.us')
+    a.assert_finds_whole_url('LOANFINDERFAST.US')
+    a.assert_finds_url('LOANFINDERFAST.us.', 'LOANFINDERFAST.us')
+    a.assert_finds_url('LOANFINDERFAST.us-', 'LOANFINDERFAST.us')
 
-    assert_finds_whole_url('http://Cervejoteca.com.br')
+
+def test_custom_regex():
+    a = RegexAssertion(url_regex_common_tlds)
+    assert_url_regex(a)
+
+    a.assert_no_url('org.openqa.selenium.support.events') # events is a tld
+    a.assert_no_url('EventFiringWebElement.click')        # click is a tld
+    a.assert_no_url('os.name')                            # name is a tld
+    a.assert_no_url('main.py')                            # py is a tld
+    a.assert_no_url('foo.sh')                            # py is a tld
+    a.assert_no_url('pid.id')                            # py is a tld
+
+    a.assert_finds_url('Check out http://I.have.com?query!', 'http://I.have.com?query')
+    a.assert_finds_whole_url('www.audrey_tsang.com')
+
 
 def test_url_regex_i18n():
+    a = RegexAssertion(url_regex)
+
     # We now know the list of non-ascii top-level domains as well:
-    assert_finds_url(
+    a.assert_finds_url(
         u'This is a website: 中国互联网络信息中心.中国. No, really.',
         u'中国互联网络信息中心.中国'
     )
     # We can't find internationalized TLDs in encoded strings, however.
     if not six.PY3:  # Regexes aren't valid for both bytes and str in py3
-        assert_no_url(u'This is a website: 中国互联网络信息中心.中国. No, really.'.encode('UTF-8'))
+        a.assert_no_url(u'This is a website: 中国互联网络信息中心.中国. No, really.'.encode('UTF-8'))
 
     # But we should be able to find the punycoded TLDs in both cases:
-    assert_finds_url(
+    a.assert_finds_url(
         u'This is a website: 中国互联网络信息中心.xn--fiqs8s. No, really.',
         u'中国互联网络信息中心.xn--fiqs8s'
     )
     if not six.PY3:  # Regexes aren't valid for both bytes and str in py3
-        assert_finds_url(
+        a.assert_finds_url(
             u'This is a website: 中国互联网络信息中心.xn--fiqs8s. No, really.'.encode('UTF-8'),
             u'中国互联网络信息中心.xn--fiqs8s'.encode('UTF-8')
         )
--- yelp_uri/search.py
+++ yelp_uri/search.py
@@ -2,86 +2,90 @@
 import re
 
 from yelp_uri import RFC3986
-from .tlds import tlds
+from .tlds.all import all_tlds
+from .tlds.common import common_tlds
 
-# A regex for finding urls in free-form text.
-# This regex is space-indented, so that it looks OK on ReviewBoard
-#
-# the following regex works like this:
-# first part of URL is either:
-#   http:// or https:// followed by any dotted hostname
-#    or
-#   dotted hostname where last part is com, net, org, etc. (so we don't accidentally linkify wrong stuff)
-#    and in this case, the hostname cannot be preceded by dot or @ (or alphanum), as this causes weird matches
-#
-# (optional) second part of URL can consist of lots of differrent characters,
-#  but cannot end in .!,?;:( because those are maybe not meant to be part of the URL.
-# NOTE: regex has been recently modified to allow a url to end in ")" so it can support urls
-# that contain parenthesis, such as wikipedia urls (e.g. 'http://en.wikipedia.org/wiki/Ham_(disambiguation)')
-# linkify_url has also been changed to look for urls ending in a ")" that do not have a matching "(", and moves
-# it outside of the closing link tag if found.
-url_regex = re.compile(
-    r"""
-        # Don't start in the middle of something.
-        (?<!  [\w./:-] )
-        (?!  mailto: )
-        # Looking for a domain name
-        (
-            # we look for a known prefix
+def create_url_regex(rfc3986, tlds):
+    # A regex for finding urls in free-form text.
+    # This regex is space-indented, so that it looks OK on ReviewBoard
+    #
+    # the following regex works like this:
+    # first part of URL is either:
+    #   http:// or https:// followed by any dotted hostname
+    #    or
+    #   dotted hostname where last part is com, net, org, etc. (so we don't accidentally linkify wrong stuff)
+    #    and in this case, the hostname cannot be preceded by dot or @ (or alphanum), as this causes weird matches
+    #
+    # (optional) second part of URL can consist of lots of differrent characters,
+    #  but cannot end in .!,?;:( because those are maybe not meant to be part of the URL.
+    # NOTE: regex has been recently modified to allow a url to end in ")" so it can support urls
+    # that contain parenthesis, such as wikipedia urls (e.g. 'http://en.wikipedia.org/wiki/Ham_(disambiguation)')
+    # linkify_url has also been changed to look for urls ending in a ")" that do not have a matching "(", and moves
+    # it outside of the closing link tag if found.
+    return re.compile(
+        r"""
+            # Don't start in the middle of something.
+            (?<!  [\w./:-] )
+            (?!  mailto: )
+            # Looking for a domain name
             (
-                https?://
-            |
-                www\.
-            )
-            ([^%(not_userinfo)s]+@)? # maybe a user?
-            [^%(not_regname)s]+
-            \.
-            [^%(not_regname)s.]{2,}
-            (:\d+)? # maybe a port?
-        |
-            # Don't start in the middle of an email
-            (?<!  @ )
-            # or else look for a domain name with a known suffix.
-            # We're more strict about dots / userinfo in this case, since the
-            # user intent is more ambiguous.
-            (   # one or more domain segments
-                [^%(not_regname)s.]+\.
-            )+
-            (%(tlds)s)
-            (:\d+)? # maybe a port?
-        )
-        # An optional path/query/fragment component
-        (
-            [/?#]
-            (
-                # Figure out if we have parens in our url
-                (?=(?P<HAS_PARENS>
-                    [^:%(not_url)s]*\(
-                ))?
-                (?(HAS_PARENS)
-                    # If we have parens, we use this:
-                    [^%(not_url)s]*
-                    [^%(bad_end)s]
+                # we look for a known prefix
+                (
+                    https?://
                 |
-                    # Otherwise, we use this:
-                    [^\)%(not_url)s]*
-                    [^\)%(bad_end)s]
+                    www\.
                 )
+                ([^%(not_userinfo)s]+@)? # maybe a user?
+                [^%(not_regname)s]+
+                \.
+                [^%(not_regname)s.]{2,}
+                (:\d+)? # maybe a port?
+            |
+                # Don't start in the middle of an email
+                (?<!  @ )
+                # or else look for a domain name with a known suffix.
+                # We're more strict about dots / userinfo in this case, since the
+                # user intent is more ambiguous.
+                (   # one or more domain segments
+                    [^%(not_regname)s.]+\.
+                )+
+                (%(tlds)s)
+                (:\d+)? # maybe a port?
+            )
+            # An optional path/query/fragment component
+            (
+                [/?#]
+                (
+                    # Figure out if we have parens in our url
+                    (?=(?P<HAS_PARENS>
+                        [^:%(not_url)s]*\(
+                    ))?
+                    (?(HAS_PARENS)
+                        # If we have parens, we use this:
+                        [^%(not_url)s]*
+                        [^%(bad_end)s]
+                    |
+                        # Otherwise, we use this:
+                        [^\)%(not_url)s]*
+                        [^\)%(bad_end)s]
+                    )
+                )?
             )?
-        )?
-        # Look-ahead to make sure the URL ends nicely.
-        (?=
-            [\)%(bad_end)s]
-        |
-            $
-        )
-    """ % dict(
-        vars(RFC3986.re),
-        tlds=tlds,
-    ),
-    re.VERBOSE | re.UNICODE | re.IGNORECASE,
-)
+            # Look-ahead to make sure the URL ends nicely.
+            (?=
+                [\)%(bad_end)s]
+            |
+                $
+            )
+        """ % dict(
+            vars(rfc3986.re),
+            tlds=tlds,
+        ),
+        re.VERBOSE | re.UNICODE | re.IGNORECASE,
+    )
 
+url_regex = create_url_regex(RFC3986, all_tlds)
+url_regex_common_tlds = create_url_regex(RFC3986, common_tlds)
 
 # A regex for finding email addresses in free-form text.
 email_regex = re.compile(
